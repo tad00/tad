cd439041f5e038d1253b56676c0c842a4206ce37
tad00 <theanh97pro@gmail.com>
2018-04-18 18:46:27 +0700

    done..

---
+++ Person.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Sup_kw4;
+
+/**
+ *
+ * @author thean
+ */
+public class Person {
+    int id;
+    String name;
+    Person(int i, String n){
+        id= i;
+        name = n;
+    }
+}
