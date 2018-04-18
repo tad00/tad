544fa4f1f7030016953a0069b31bb54670ca4869
tad00 <theanh97pro@gmail.com>
2018-04-18 18:58:26 +0700

    done..

---
+++ Main.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Sup_kw4;
+
+/**
+ *
+ * @author thean
+ */
+public class Main {
+    public static void main(String[] args) {
+        Emp e1=new Emp(1,"ankit",45000f);  
+        e1.display(); 
+    }
+}
